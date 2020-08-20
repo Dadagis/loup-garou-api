33e7bd9ba1e06ebbde86e661141f251773325cfd
David <davidobob@laposte.net>
2020-08-20 14:20:08 +0200

    put route added

--- routes/games.js
+++ routes/games.js
@@ -36,4 +36,33 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const game = await Game.findById(req.params.id);
+  if (!game) {
+    res.status(404).send("The game with the given ID was not found");
+    return;
+  }
+
+  const { error } = validateGame(req.body);
+
+  if (error) {
+    res
+      .status(400)
+      .send(
+        `Expected ${error.details[0].message} ; was "${error.details[0].context.value}"`
+      );
+    return;
+  }
+
+  game.name = req.body.name;
+  game.players = req.body.players;
+
+  try {
+    const result = await game.save();
+    res.send(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 module.exports = router;

a77df90d2253d7133dee12753759451a9bc6c6b9
David <davidobob@laposte.net>
2020-08-20 13:54:34 +0200

    game model created

---
+++ models/game.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Joi = require("joi");
+
+const gameSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+  players: {
+    type: Array,
+    required: true,
+  },
+  winner: String,
+});
+
+const Game = mongoose.model("Game", gameSchema);
+
+const validateGame = (game) => {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    players: Joi.array().required(),
+    winner: Joi.string(),
+  });
+  return schema.validate(game);
+};
+
+exports.Game = Game;
+exports.validateGame = validateGame;

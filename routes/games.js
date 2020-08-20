fb9306aa147a0a5c5f0319528a53745cb61bce42
David <davidobob@laposte.net>
2020-08-20 14:06:21 +0200

    implemented new game route

---
+++ routes/games.js
@@ -0,0 +1,39 @@
+const { Game, validateGame } = require("../models/game");
+const express = require("express");
+const router = express.Router();
+
+router.get("/:id", async (req, res) => {
+  const game = await Game.findById(req.params.id);
+  if (!game) {
+    res.status(404).send("The game with the given ID was not found !");
+  } else {
+    res.send(game);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validateGame(req.body);
+
+  if (error) {
+    res
+      .status(400)
+      .send(
+        `Expected ${error.details[0].message} ; was "${error.details[0].context.value}"`
+      );
+    return;
+  }
+
+  const game = new Game({
+    name: req.body.name,
+    players: req.body.players,
+  });
+
+  try {
+    const result = await game.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const home = require("../routes/home");
+const games = require("../routes/games");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/", home);
+  app.use("/api/games", games);
 };

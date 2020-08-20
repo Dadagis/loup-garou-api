989ce927e1afe49e68545fc4c2e56934e726d6c5
David <davidobob@laposte.net>
2020-08-20 14:57:55 +0200

    added auth middleware + /me route, still have to implement authentication

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,12 @@
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const { User, validateUser } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
+
+module.exports = router;
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const home = require("../routes/home");
 const games = require("../routes/games");
+const users = require("../routes/users");
 
 module.exports = function (app) {
   app.use(express.json());
   app.use("/", home);
   app.use("/api/games", games);
+  app.use("/api/users", users);
 };

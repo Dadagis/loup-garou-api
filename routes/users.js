464ec3e61376a8c5f765bd5eedd4bd9cba243440
David <davidobob@laposte.net>
2020-08-21 10:25:03 +0200

    user auth

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,40 @@ router.get("/me", auth, async (req, res) => {
   res.send(user);
 });
 
+router.post("/", async (req, res) => {
+  const { error } = validateUser(req.body);
+
+  if (error) {
+    res
+      .status(400)
+      .send(
+        `Expected ${error.details[0].message} ; was "${error.details[0].context.value}"`
+      );
+    return;
+  }
+
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered");
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
+
+  const token = user.generateAuthToken();
+
+  try {
+    await user.save();
+    res
+      .header("x-auth-token", token)
+      .send(_.pick(user, ["id", "name", "email"]));
+  } catch (error) {
+    console.log(error.message);
+  }
+});
+
 module.exports = router;

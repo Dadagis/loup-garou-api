2a405889c7303f5eb27939ae9bbc65e9056ac06b
David <davidobob@laposte.net>
2020-08-21 11:36:00 +0200

    little clean

--- routes/games.js
+++ routes/games.js
@@ -4,9 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/:id", auth, Game.findById);
-
 router.post("/", auth, Game.create);
-
 router.put("/:id", auth, Game.update);
 
 module.exports = router;

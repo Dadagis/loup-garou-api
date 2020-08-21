44ba53a2c77482677d67e4f5ef058c733b1f1b8b
David <davidobob@laposte.net>
2020-08-21 10:48:10 +0200

    made games routes auth required

--- routes/games.js
+++ routes/games.js
@@ -1,17 +1,22 @@
+const auth = require("../middlewares/auth");
 const { Game, validateGame } = require("../models/game");
 const express = require("express");
 const router = express.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   const game = await Game.findById(req.params.id);
   if (!game) {
     res.status(404).send("The game with the given ID was not found !");
   } else {
-    res.send(game);
+    try {
+      res.send(game);
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validateGame(req.body);
 
   if (error) {
@@ -36,7 +41,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const game = await Game.findById(req.params.id);
   if (!game) {
     res.status(404).send("The game with the given ID was not found");
